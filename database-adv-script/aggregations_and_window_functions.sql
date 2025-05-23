b205cdb2297e9ce040673a5976cb0ec13f5534f3
baydre_africa <baydreafrica@gmail.com>
2025-05-23 13:50:06 +0100

    feat: Applied Aggregations and Window Functions

---
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -0,0 +1,20 @@
+-- Total bookings made by each user
+SELECT
+  user_id,
+  COUNT(*) AS total_bookings
+FROM
+  bookings
+GROUP BY
+  user_id
+ORDER BY
+  total_bookings DESC;
+
+-- Ranking properties by the number of bookings
+SELECT
+  property_id,
+  COUNT(*) AS booking_count,
+  RANK() OVER (ORDER BY COUNT(*) DESC) AS booking_rank
+FROM
+  bookings
+GROUP BY
+  property_id;
