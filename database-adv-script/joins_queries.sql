593f2604afb604b64bd8f5d5ba7d9d5b02c5020f
baydre_africa <baydreafrica@gmail.com>
2025-05-23 14:01:37 +0100

    chore: Added `ORDER BY` clause

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -30,7 +30,8 @@ SELECT
   r.rating,
   r.comment
 FROM properties p
-LEFT JOIN reviews r ON p.property_id = r.property_id;
+LEFT JOIN reviews r ON p.property_id = r.property_id
+ORDER BY p.property_id;
 
 ---------------------------------
 
